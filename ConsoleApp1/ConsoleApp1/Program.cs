ba5591423384c3737763b8995c2724040bd0bbda
maseh <Gebruiker@DESKTOP-0J252BE>
2022-04-19 19:12:07 +0200

    aantal personen check

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,44 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace Aantal_Bezoekers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // bezoeker moet minimaal 1 en maximaal 8 personen zijn
+
+            Console.WriteLine("Vul het aantal personen in:  ");
+            string x;
+            int person;
+            x = Console.ReadLine();
+
+            // Controleer of het letters of cijfer zijn
+            while (!x.All(char.IsDigit))
+            {
+                Console.WriteLine("Invalid value, Vul het aantal personen in: ");
+                x = Console.ReadLine();
+            }
+
+            person = int.Parse(x);
+
+
+            if (person > 0 && person < 9)
+            {
+                Console.WriteLine("We reserveren de plaats voor u voor " + person + " persoon/personen");
+
+
+            }
+            else if (person < 1)
+            {
+                Console.WriteLine("Er moet voor minimaal één persoon worden gereserveerd.");
+            }
+            else  // meer dan 8 personen
+            {
+                Console.WriteLine("Als u voor meer dan 8 personen wilt boeken, neem dan telefonisch contact met ons op.\nT: 01 7245 754");
+            }
+        }
+
+    }
+}
