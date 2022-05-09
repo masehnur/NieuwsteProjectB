7bc51ba4de7e6b15e99926f028b3db8665d55c92
Kangyou123 <80524495+Kangyou123@users.noreply.github.com>
2022-05-09 11:26:15 +0200

    Add files via upload

---
+++ Reservering_project/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Part2
+
+{
+    public class Reservation
+    {
+        public readonly int KlantID;
+
+        public readonly int TafelID;
+
+        public readonly int AantalPersonen;
+
+        public readonly DateTime ReserveringTijd;
+
+        public Reservation(int klantID, int tafelID, int aantalPersonen, DateTime reserveringTijd)
+        {
+            KlantID = klantID;
+            TafelID = tafelID;
+            AantalPersonen = aantalPersonen;
+            ReserveringTijd = reserveringTijd;
+        }
+    }
+}
