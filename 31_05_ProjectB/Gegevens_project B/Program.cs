a0658b0de70ddcf10356af882ad89daafdd82037
Ahmad Alhaj Asaad <ahmadalhajasaad1982@gmail.com>
2022-06-06 21:25:24 +0200

    27 text veranderd

--- 31_05_ProjectB/Gegevens_project B/Program.cs
+++ 31_05_ProjectB/Gegevens_project B/Program.cs
@@ -24,7 +24,7 @@ namespace Reservering
             //menu/inloggen/registreren/verder als gast
             #region 1
 
-            // Menu weergeven  probeer push
+            // Menu weergeven
             Console.Clear();
             Console.WriteLine("\t\t\t--<<<Welkom in ons restaurant>>>--\n");
             Console.WriteLine("\t\t 1 - Registeren");
